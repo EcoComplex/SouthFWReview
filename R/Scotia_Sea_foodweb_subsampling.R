2f9a428a499e5cdc89f538ac127e4759cb62cc39
Kortsch <susakort@ad.helsinki.fi>
2023-06-06 15:29:12 +0300

    new files

--- R/Scotia_Sea_foodweb_subsampling.R
+++ R/Scotia_Sea_foodweb_subsampling.R
@@ -1,17 +1,11 @@
 ###Scotia Sea food web###
 
-#working dir
-#C:\LocalData\susakort\GitHub\PIP\Rscripts
-setwd("C:/LocalData/susakort/GitHub/PIP/Rscripts")
-
-dirF<-"../Figures/"
-dirSoctia<-"../Data/Scotia_Sea/"
 
 #Scotia nodes info
 #metaweb_nodes.csv
-nodes_Scotia<-read.csv(paste0(dirSoctia,"metaweb_nodes.csv"), sep=",")
+nodes_Scotia<-read.csv("Data/metaweb_nodes.csv", sep=",")
 dim(nodes_Scotia)
-nodes_Scotia<-nodes_Scotia[,-c(8,9)]
+nodes_Scotia<-nodes_Scotia[,-c(8,9)]#remove these columns
 head(nodes_Scotia)
 
 #southern_nodes
@@ -27,7 +21,7 @@ head(NS_pw)
 dim(NS_pw)
 
 #Scotia links metaweb
-links_scotia<-read.csv(paste0(dirSoctia,"metaweb_links.csv"), sep=",")
+links_scotia<-read.csv("Data/metaweb_links.csv", sep=",")
 head(links_scotia)
 
 #Clean names of the pl, full species list
@@ -43,7 +37,7 @@ plA <- plA[!duplicated(plA),]
 dim(plA) # rows are identical to number of trophic links
 str(plA)
 SS_FW<-plA
-write.csv(SS_FW, "../Data/Scotia_Sea/SouthernScotia_FoodWeb.csv", row.names=T)
+write.csv(SS_FW, "Data/SouthernScotia_FoodWeb.csv", row.names=F)
 
 #subset the pairwise list for Northern Scotia Sea
 pl_cons2 <- subset(links_scotia, links_scotia$consumer%in%NS_pw$node)
@@ -54,7 +48,39 @@ dim(plA2) # rows are identical to number of trophic links
 str(plA2)
 NS_FW<-plA2
 head(NS_FW)
-write.csv(NS_FW, "../Data/Scotia_Sea/NorthernScotia_FoodWeb.csv", row.names=T)
+write.csv(NS_FW, "Data/NorthernScotia_FoodWeb.csv", row.names=F)
+
+
+#Extract taxonmic information for species
+
+library("taxize")
+library("tidyverse")
+#scotia_tax<-tax_name(nodes_Scotia$node, db = "itis", get = c("genus", "family", "order", "class", "phylum"))
+
+#Scotia Sea taxonmic info 
+scotia_tax_list<-read_csv("Data/scotia_taxonomic_list.csv")
+scotia_tax_list<-scotia_tax_list[, -c(1,2)]
+colnames(scotia_tax_list)<-c("species", "genus", "family", "order", "class", "phylum")
+#head(scotia_tax_list)
+#unique(scotia_tax_list$order)
+
+#
+ss_fw<-read_csv("Data/SouthernScotia_FoodWeb.csv")
+head(ss_fw)
+dim(ss_fw)
+dfn_ss <- unique(dtot$consumer)
+con_names_ss <- gnr_resolve(dfn_ss, best_match_only = TRUE, canonical=TRUE)
+# Species not found
+anti_join(data.frame(user_supplied_name=dfn_ss),con_names_ss)
+
+
+dfn_ss2 <- unique(dtot$resource)
+res_names <- gnr_resolve(dfn_ss2, best_match_only = TRUE, canonical=TRUE)
+anti_join(data.frame(user_supplied_name=dfn_ss2),res_names)
+
+# pairwise interaction list for the Southern Scotia Sea food web
+gM <- graph_from_edgelist(as.matrix(ss_fw), directed  = T)
+
 
 
 
