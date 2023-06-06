0e59ca3084d886cd35cdf97d1f8e965a1cb97022
TomasMarina <tomasimarina@gmail.com>
2023-06-06 17:48:02 +0300

    Comparing FW versions

---
+++ R/Compare_FW_versions.R
@@ -0,0 +1,39 @@
+##
+# Compare original & standardized FWs
+##
+
+library(tidyverse)
+library(igraph)
+library(multiweb)
+
+# Scotia Sea ----
+# Old
+nscotia_old <- read.csv("Data/NorthernScotia_FoodWeb.csv", header = T)
+sscotia_old <- read.csv("Data/SouthernScotia_FoodWeb.csv", header = T)
+# Standardized
+nscotia_std <- read.csv("Data/NorthernScotia_top.csv", header = T, colClasses = c("NULL",NA,NA))
+sscotia_std <- read.csv("Data/SouthernScotia_top.csv", header = T, colClasses = c("NULL",NA,NA))
+
+# Igraph objects
+nscotia_old_g <- graph_from_data_frame(nscotia_old, directed = T)
+sscotia_old_g <- graph_from_data_frame(sscotia_old, directed = T)
+nscotia_std_g <- graph_from_data_frame(nscotia_std, directed = T)
+sscotia_std_g <- graph_from_data_frame(sscotia_std, directed = T)
+
+# Properties
+nscotia_old_prop <- calc_topological_indices(nscotia_old_g) %>% mutate(Name="Old N Scotia")
+sscotia_old_prop <- calc_topological_indices(sscotia_old_g) %>% mutate(Name="Old S Scotia")
+nscotia_std_prop <- calc_topological_indices(nscotia_std_g) %>% mutate(Name="Std N Scotia")
+sscotia_std_prop <- calc_topological_indices(sscotia_std_g) %>% mutate(Name="Std S Scotia")
+
+scotia_prop <- bind_rows(nscotia_old_prop, sscotia_old_prop, nscotia_std_prop, sscotia_std_prop) %>% 
+  rename(Network=Name) %>% 
+  dplyr::select(Network, everything())
+
+# Plot
+par(mfrow = c(2,2))
+plot_troph_level(nscotia_old_g, main = "Old N Scotia")
+plot_troph_level(sscotia_old_g, main = "Old S Scotia")
+plot_troph_level(nscotia_std_g, main = "Std N Scotia")
+plot_troph_level(sscotia_std_g, main = "Std S Scotia")
+
